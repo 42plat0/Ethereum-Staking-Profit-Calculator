63ed89a578951b3acca370cc08e728dd9028e78c
Gypra <gytisnaujasis@icloud.com>
2024-12-03 12:26:06 +0200

    Add error checking for helper function saving csv

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -2,16 +2,20 @@ import csv
 
 def save_csv(file_path: str, reward_log: list[dict], separator:str):
     
-    with open(file_path, "w", newline="", encoding="utf-8") as output_file:
+    try:
+        with open(file_path, "w", newline="", encoding="utf-8") as output_file:
 
-        header = reward_log[0].keys()
-        
-        writer = csv.writer(output_file, delimiter=separator)
-        # Write separator to properly display data
-        writer.writerow([f"sep={separator}"])
+            header = reward_log[0].keys()
+            
+            writer = csv.writer(output_file, delimiter=separator)
+            # Write separator to properly display data
+            writer.writerow([f"sep={separator}"])
 
-        writer.writerow(header)
+            writer.writerow(header)
 
-        # Loop through records to get their values associated with keys
-        for i in range(len(reward_log)):
-            writer.writerow(reward_log[i].values())
+            # Loop through records to get their values associated with keys
+            for i in range(len(reward_log)):
+                writer.writerow(reward_log[i].values())
+    except PermissionError as e:
+        raise PermissionError("Close file before creating a new one or check if directory exists")    
+    
